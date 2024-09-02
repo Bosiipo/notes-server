3c079929b818a9d60b933af8a08c8d3f7f726958
Bosipo <bosiposhogbolu@gmail.com>
2024-09-02 04:25:57 +0100

    Update database url update check

--- build/src/middleware/validateBody.d.ts
+++
@@ -1,5 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-import { AnySchema } from "yup";
-type Middleware = (req: Request, res: Response, next: NextFunction) => Promise<Response<any> | void>;
-export default function (schema: AnySchema): Middleware;
-export {};
--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,10 @@ dotenv.config();
 import mongoose from "mongoose";
 const { DATABASE_URL } = process.env;
 
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set.");
+}
+
 export default async () => {
   try {
     await mongoose.connect(DATABASE_URL);
